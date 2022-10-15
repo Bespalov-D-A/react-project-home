83994cc746ce5229edac9188022bcdf02a364840
leoleq <googlonator@gmail.com>
2022-10-15 22:00:45 +0400

    Step 1. Will be create html

--- src/App.js
+++ src/App.js
@@ -1,24 +1,98 @@
-import logo from './logo.svg';
-import './App.css';
+import "./sass/App.scss";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header className="prj-header">
+        <div className="prj-wrap">
+          <div className="prj-wrap__left">
+            <h1 className="prj-header-title prj-header-title--gray">
+              Здравствуйте,
+            </h1>
+          </div>
+          <div className="prj-wrap__right">
+            <h1 className="prj-header-title">Человек №3596941</h1>
+            <span className="prj-header-link">
+              <a className="prj-header-link__link" href="">
+                Сменить статус
+              </a>
+            </span>
+          </div>
+        </div>
+        <div className="prj-wrap">
+          <div className="prj-wrap__left"></div>
+          <div className="prj-wrap__right">
+            <div className="prj-header-stat">
+              <p className="prj-header-stat__status">
+                Прежде чем действовать, надо понять
+              </p>
+            </div>
+          </div>
+        </div>
       </header>
-    </div>
+      <main className="prj-main">
+        <div className="prj-wrap">
+          <div className="prj-wrap__left">
+            <p className="prj-input-name">Ваш город</p>
+          </div>
+          <div className="prj-wrap__right">
+            <input className="prj-input prj-input--select" type="select" />
+            <p className="prj-input-desc"></p>
+          </div>
+        </div>
+        <div className="prj-wrap">
+          <div className="prj-wrap__left">
+            <p className="prj-input-name">Пароль</p>
+          </div>
+          <div className="prj-wrap__right">
+            <input className="prj-input" type="select" />
+            <p className="prj-input-desc">
+              Ваш новый пароль должен содержать не менее 5 символов.
+            </p>
+          </div>
+        </div>
+        <div className="prj-wrap">
+          <div className="prj-wrap__left">
+            <p className="prj-input-name">Пароль еще раз</p>
+          </div>
+          <div className="prj-wrap__right">
+            <input className="prj-input" type="select" />
+            <p className="prj-input-desc">
+              Повторите пароль, пожалуйста, это обезопасит вас с нами на случай
+              ошибки.
+            </p>
+          </div>
+        </div>
+        <div className="prj-wrap">
+          <div className="prj-wrap__left">
+            <p className="prj-input-name">Электронная почта</p>
+          </div>
+          <div className="prj-wrap__right">
+            <input className="prj-input" type="select" />
+            <p className="prj-input-desc">
+              Можно изменить адрес, указанный при регистрации.
+            </p>
+          </div>
+        </div>
+
+        <div className="prj-wrap">
+          <div className="prj-wrap__left">Я согласен</div>
+          <div className="prj-wrap__right">
+            <input className="prj-input-checkbox" type="checkbox" />
+            <p className="prj-input-desc prj-input-desc--black">
+              принимать актуальную информацию на емейл
+            </p>
+          </div>
+        </div>
+        <div className="prj-wrap">
+          <div className="prj-wrap__left"></div>
+          <div className="prj-wrap__right">
+          <button className="prj-btn">Изменить</button>
+            <p className="prj-input-desc">последние изменения 15 мая 2012 в 14:55:17</p>
+          </div>
+        </div>
+      </main>
+    </>
   );
 }
 
